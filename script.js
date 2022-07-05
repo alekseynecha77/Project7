42b1486c581a61b6dda79158c13bb959f7717590
alekseynecha77 <68791158+alekseynecha77@users.noreply.github.com>
2022-07-04 21:13:29 -0400

    small updates

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
-document.getElementById("bell").onmouseover = function () {
-    var myList = document.getElementById("navSect");
+// document.getElementById("bell").onmouseover = function () {
+//     var myList = document.getElementById("navSect");
   
-    myList.innerHTML='';
+//     myList.innerHTML='';
 
-    };
+//     };
