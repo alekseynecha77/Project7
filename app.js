eb610b6beb34dbd5117523d6caa87e5831afdc2b
alekseynecha77 <68791158+alekseynecha77@users.noreply.github.com>
2022-07-21 20:19:56 -0400

    make format prettier

--- app.js
+++ app.js
@@ -1,41 +1,7 @@
-// const alertBanner = document.getElementById("alert");
-// const bell = document.getElementsByClassName('bell-svg');
-
-
-
- /*   function myFunction() {
-      var popup = document.getElementsByClassName("collapsible");
-      var i;
-   
-      bell.innerHTML = `
-   
-      <div class="content">
-        <p>Dale Byrd liked the post  <strong>Facebook's Changes for 2021</strong></p>
-      </div>
-      
-`
-
- for(i=0; i<popup.length;i++){
-   popup[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var content = this.nextElementSibling;
-      if(content.style.maxHeight){
-         content.style.maxHeight = null;
-      }else{
-         content.style.maxHeight = content.scrollHeight + "px";
-
-      }
-
- });
-
-
- }
-   } */
-
-   let popup = document.getElementById("popup");
-   function openPopup(){
-      popup.classList.add("open-popup")
-   }
-   function closePopup(){
-      popup.classList.remove("open-popup")
-   }
+let popup = document.getElementById("popup");
+function openPopup() {
+  popup.classList.add("open-popup");
+}
+function closePopup() {
+  popup.classList.remove("open-popup");
+}
