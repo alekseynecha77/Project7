c589a54c811eaebf556a40289c5d4e661439a110
alekseynecha77 <68791158+alekseynecha77@users.noreply.github.com>
2022-07-26 21:34:20 -0400

    update

--- app.js
+++ app.js
@@ -5,3 +5,24 @@ function openPopup() {
 function closePopup() {
   popup.classList.remove("open-popup");
 }
+
+let ToSend = document.getElementById('send');
+
+ToSend.addEventListener('click', () =>{
+ ToSend.innerHTML = `  
+ <body id="bodyMEMBER">
+ <div class="members-cc">
+ <div class='members-container'>
+     <img src="images/member-1.jpg" class="member-photo" alt="member photo">
+     <div class="member-info">
+         <p class="MName">Victoria Chambers</p>
+         <a href="#">victoria.chambers@example.com</a>
+     </div>
+     <p>02/12/22</p> 
+ </div>
+ </body>
+`
+
+document.getElementById('bodyMEMBER').style.display = "block";
+
+})
