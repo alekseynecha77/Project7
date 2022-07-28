7b9d3ff7f902a7079420ae5ba7df099f27fe2ad6
alekseynecha77 <68791158+alekseynecha77@users.noreply.github.com>
2022-07-27 20:07:52 -0400

    update

--- app.js
+++ app.js
@@ -6,23 +6,31 @@ function closePopup() {
   popup.classList.remove("open-popup");
 }
 
-let ToSend = document.getElementById('send');
+// let ToSend = document.getElementById('send');
 
-ToSend.addEventListener('click', () =>{
- ToSend.innerHTML = `  
- <body id="bodyMEMBER">
- <div class="members-cc">
- <div class='members-container'>
-     <img src="images/member-1.jpg" class="member-photo" alt="member photo">
-     <div class="member-info">
-         <p class="MName">Victoria Chambers</p>
-         <a href="#">victoria.chambers@example.com</a>
-     </div>
-     <p>02/12/22</p> 
- </div>
- </body>
-`
+// ToSend.addEventListener('click', () =>{
+//  ToSend.innerHTML = `  
+//  <body id="bodyMEMBER">
+//  <div class="members-cc">
+//  <div class='members-container'>
+//      <img src="images/member-1.jpg" class="member-photo" alt="member photo">
+//      <div class="member-info">
+//          <p class="MName">Victoria Chambers</p>
+//          <a href="#">victoria.chambers@example.com</a>
+//      </div>
+//      <p>02/12/22</p> 
+//  </div>
+//  </body>
+// `
 
-document.getElementById('bodyMEMBER').style.display = "block";
+// document.getElementById('bodyMEMBER').style.display = "block";
 
-})
+// })
+
+// function sendFunc(){
+// if(){
+
+
+// }else if(){}
+
+// }
