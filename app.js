58ee6a03511aad62223d46caab9ea010db1ee4a1
alekseynecha77 <68791158+alekseynecha77@users.noreply.github.com>
2022-08-03 16:27:45 -0400

    update

--- app.js
+++ app.js
@@ -92,3 +92,15 @@ btnSetting.addEventListener('click', () => {
 
 
 
+let elements = document.querySelectorAll('.product-list')
+
+elements.forEach(el => {
+  el.addEventListener('click', () => {
+    elements.forEach(el => el.classList.remove('active'))
+    el.classList.add('active')
+  })})
+
+
+
+
+
